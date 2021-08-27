6c17841b34dcf8fad3ae50f8b9973fbf1554139a
Aleksandr <djomadjomich@gmail.com>
2021-08-27 22:51:26 +0200

    Task7

---
+++ Lesson7/Assets/Scripts/ListTElementCount.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lesson7
+{
+    public static class ListTElementCount
+    {
+        static List<int> numbers = new List<int>(){ 0,2,2,1,2,3 };
+        
+        // numbers.Sort();
+        //
+        // static Dictionary<int, int> result = new Dictionary<int,int>();
+        // for(int i = 1; i < numbers.Count; i++)
+        // {
+        //     if (numbers[i] == numbers[i - 1]);
+        // }
+        // foreach (int val in numbers.Distinct())
+        // {
+        // Debug.Log(val + " - " + numbers.Where(x => x == val).Count() + " раз");
+        // }
+    }
+}
